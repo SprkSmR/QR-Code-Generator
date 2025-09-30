f098c497e0dd6c2399d9b28b170f2f01f05ab362
SprkSmR <chitox_ji_armen@hotmail.com>
2025-09-30 06:08:13 -0600

    Done up to TODO 2

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+import inquirer from 'inquirer';
+import * as fs from 'fs';
+import * as qr from 'qr-image';
+
+inquirer
+    .prompt([
+        {
+            name: 'linkToQR',
+            message: 'Please enter the link to convert into QR'
+        },
+    ])
+    .then(answers => {
+        var linkQR = qr.image(answers.linkToQR);
+        linkQR.pipe(fs.createWriteStream('linkQR.png'));
+    });
+
 /* 
-1. Use the inquirer npm package to get user input.
-2. Use the qr-image npm package to turn the user entered URL into a QR code image.
 3. Create a txt file to save the user input using the native fs node module.
 */
